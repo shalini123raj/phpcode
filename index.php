1804f00ed867dec04523b40e7f0297b6527084bd
shalini <shalinibegusarai8@gmail.com>
2025-05-20 16:30:20 +0530

    complete

--- index.php
+++ index.php
@@ -1,101 +1,282 @@
 <!DOCTYPE html>
 
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-     <title>Total Yes Eligible</title>
+    <title>Total Yes Eligible</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-   <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./style.css">
 
 </head>
 
 <body>
 
-    
 
 
-<?php
 
-$students = array(
-    array("name" => "Ajay", "Age" => 25, "salary" => 20000),
-    array("name" => "Amit", "Age" => 0, "salary" => 25000 ),
-    array("name" => "Sunny", "Age" => 14, "salary" => 23000 ),
-    array("name" => "Radha", "Age" => 27, "salary" => 27000),
-    array("name" => "Nikhil", "Age" => 15, "salary" => 24000 ),
-    array("name" => "Isreal", "Age" => 22, "salary" => 22000 ),
-    array("name" => "shalini", "Age" => 21, "salary" =>20000 ),
-    array("name" => "shalini", "Age" => 5, "salary" =>20000 ),
-    array("name" => "shalini", "Age" => 21, "salary" =>20000 ),
-    array("name" => "shalini", "Age" => 17, "salary" =>20000 ),
-    array("name" => "shalini", "Age" => 21, "salary" =>20000 ),
-);  
+    <?php
 
-
-?>
+    $students = array(
+        array("name" => "Ajay", "Age" => 25, "salary" => 20000),
+        array("name" => "Amit", "Age" => 0, "salary" => 5),
+        array("name" => "Sunny", "Age" => 14, "salary" => 5000),
+        array("name" => "Radha", "Age" => 27, "salary" => 27000),
+        array("name" => "Nikhil", "Age" => 15, "salary" => 24000),
+        array("name" => "Isreal", "Age" => 22, "salary" => 45000),
+        array("name" => "shalini", "Age" => 21, "salary" => 20000),
+        array("name" => "shalini", "Age" => 5, "salary" => 35000),
+        array("name" => "shalini", "Age" => 21, "salary" => 20000),
+        array("name" => "shalini", "Age" => 17, "salary" => 20000),
+        array("name" => "shalini", "Age" => 21, "salary" => 20000),
+    );
 
 
-<table style="width: 100%">
-    
-    <tr>
-        <th>name</th>
-        <th>age</th>
-        <th>salary</th>
-        <th>Elg.Vote</th>
+    ?>
+
+    <h1>table 1</h1>
+
+
+    <table style="width: 100%">
+
+        <tr>
+            <th>name</th>
+            <th>age</th>
+            <th>salary</th>
+            <th>Elg.Vote</th>
+            <th>Salary type</th>
+
+
+
+        </tr>
+        <?php
+        $a = 0;
+        $salary = 0;
+        $yesCount = 0;
+        $noCount = 0;
+        $basic = $medium = $high = "";
+
+
+        foreach ($students as  $i => $s) {
+            $a = $a + $s['Age'];
+            $salary = $salary + $s['salary'];
+
+
+           if ($s['Age'] >= 18) {
+
+                $yesCount++;
+            } else {
+
+                $noCount++;
+            }
+
+            $salrytype = '';
+            if ($s['salary'] < 20000) {
+                $salrytype = 'basic';
+            } elseif ($s['salary'] >= 20000 && $s['salary'] <= 27000) {
+                $salrytype = 'medium';
+            } else {
+                $salrytype = 'high';
+            }
+
+        ?>
+
+            <tr class="<?php echo ($s['Age'] >= 18) ? 'highlight-green' : 'highlight-red'; ?>">
+
+                <td><?php echo $s['name']; ?></td>
+                <td><?php echo $s['Age']; ?></td>
+                <td><?php echo $s['salary']; ?></td>
+                <td><?php
+                    if ($s['Age'] >= 18) {
+                        echo  "yes";
+                    } else {
+                        echo "No ";
+                    }
+                    ?>
+                </td>
+                <td>
+                    <?php echo $salrytype; ?>
+                </td>
+
+            </tr>
+        <?php } ?>
+
+
+        <tr>
+            <td><?php echo $i + 1 ?></td>
+            <td><?php echo $a / count($students); ?></td>
+            <td><?php echo $salary / count($students); ?></td>
+            <td>
+
+
+
+                <p style=color:blue>Total yes Eligible (yes):<?php echo $yesCount ?></p>
+                <p style=color:blue>Total Not Eligible (No):<?php echo $noCount ?></p>
+            </td>
+
+
+
+        </tr>
+
+
+    </table>
+
+
+
+    <h1">table2</h1>
+
+
+    <table style="width: 100%" id="tabl12">
+
+        <tr>
+            <th style="color:chartreuse ;">name</th>
+            <th style="color: brown;">age</th>
+            <th style="color: blue;">salary</th>
+            <th style="color: aqua;">Elg.Vote</th>
+            <th style="color: red;">Salary type</th>
+
+
+
+        </tr>
+        <?php
+        $a = 0;
+        $salary = 0;
+        $yesCount = 0;
+        $noCount = 0;
+        $basic = $medium = $high = "";
+
+
+        foreach ($students as  $i => $s) {
+            $a = $a + $s['Age'];
+            $salary = $salary + $s['salary'];
+
+
+            if ($s['Age'] >= 18) {
+
+                $yesCount++;
+            } else {
+
+                $noCount++;
+            }
+
+            $salrytype = '';
+            if ($s['salary'] < 20000) {
+                $salrytype = 'basic';
+                $color='red';
+            } elseif ($s['salary'] >= 20000 && $s['salary'] <= 27000) {
+                $salrytype = 'medium';
+                $color='green';
+
+            } else {
+                $salrytype = 'high';
+                $color='blue';
+
+            }
+
+        ?>
+
+            <tr style="background-color:<?php echo $color;?>">
+
+                <td><?php echo $s['name']; ?></td>
+                <td><?php echo $s['Age']; ?></td>
+                <td ><?php echo $s['salary']; ?></td>
+                <td><?php
+                    if ($s['Age'] >= 18) {
+                        echo  "yes";
+                    } else {
+                        echo "No ";
+                    }
+                    ?>
+                </td>
+                <td>
+                    <?php echo $salrytype; ?>
+                </td>
+
+            </tr>
+        <?php } ?>
+
+
        
-        
-    </tr>
-    <?php 
-    $a=0;
-    $salary=0;
-    $yesCount = 0;
-    $noCount = 0;
 
- 
-     foreach ($students as  $i=> $s) { 
-         $a=$a+$s['Age'];
-         $salary=$salary+$s['salary'];?>
+    </table>
+
+    <h1">table3</h1>
+
+
+    <table style="width: 100%" id="tabl13">
+
+        <tr>
+            <th style="color:chartreuse ;">name</th>
+            <th style="color: brown;">age</th>
+            <th style="color: blue;">salary</th>
+            <th style="color: aqua;">Elg.Vote</th>
+            <th style="color: red;">Salary type</th>
+
+
+
+        </tr>
+        <?php
+        $a = 0;
+        $salary = 0;
+        $yesCount = 0;
+        $noCount = 0;
+        $basic = $medium = $high = "";
+
+
+        foreach ($students as  $i => $s) {
+            $a = $a + $s['Age'];
+            $salary = $salary + $s['salary'];
+
+
+            if ($s['Age'] >= 18) {
+
+                $yesCount++;
+            } else {
+
+                $noCount++;
+            }
+
+            $salrytype = '';
+            if ($s['salary'] < 20000) {
+                $salrytype = 'basic';
+                $color='red';
+            } elseif ($s['salary'] >= 20000 && $s['salary'] <= 27000) {
+                $salrytype = 'medium';
+                $color='green';
+
+            } else {
+                $salrytype = 'high';
+                $color='blue';
+
+            }
+
+        ?>
+
+            <tr style="background-color:<?php echo $color;?>">
+
+                <td><?php echo $s['name']; ?></td>
+                <td><?php echo $s['Age']; ?></td>
+                <td ><?php echo $s['salary']; ?></td>
+                <td><?php
+                    if ($s['Age'] >= 18) {
+                        echo  "yes";
+                    } else {
+                        echo "No ";
+                    }
+                    ?>
+                </td>
+                <td>
+                    <?php echo $salrytype; ?>
+                </td>
+
+            </tr>
+        <?php } ?>
+
+
        
-    <tr class="<?php echo ($s['Age']>18)?'highlight-green':'highlight-red';?>" >
-     
-        <td><?php  echo $s['name']; ?></td>
-        <td><?php  echo $s['Age']; ?></td>
-        <td><?php  echo $s['salary']; ?></td>
-        <td><?php  
-         if ($s['Age'] >= 18) {
-        echo  "yes";
-    } else {
-        echo "No ";
-    }
-            ?>
-        </td>
-<?php
-                if ($s['Age'] >= 18) {
-                   
-                    $yesCount++;
-                } else {
-                   
-                    $noCount++;
-                }
-                ?>          
- 
-</tr>
-<?php }?>
 
 
-<tr>
-    <td><?php echo $i+1?></td>
-    <td><?php echo $a/count($students);?></td>
-    <td><?php echo $salary/count($students);?></td>
-    <td>
-
-   
-    
-                <p style=color:blue>Total yes Eligible (yes):<?php echo $yesCount ?></p>    
-                <p style=color:blue>Total Not Eligible (No):<?php echo $noCount ?></p></td>
-    
-</tr>
-   
-</table>
 </body>
-</html> 
+
+</html>
